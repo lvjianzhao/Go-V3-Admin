cbbd8df6f7aab5a2bdaa7b5b4417893558c81cd7
lvjianzhao <lv916551516@163.com>
2023-07-15 11:41:53 +0800

    style(NavigationBar): 将左侧菜单栏中的系统设置移动到header中

--- web/src/layouts/components/NavigationBar/index.vue
+++ web/src/layouts/components/NavigationBar/index.vue
@@ -5,16 +5,30 @@
     <div class="right-menu">
       <Screenfull v-if="showScreenfull" class="right-menu-item" />
       <ThemeSwitch v-if="showThemeSwitch" class="right-menu-item" />
+      <!--      系统设置-->
       <el-dropdown class="right-menu-item">
-        <el-button plain
-          >{{ userStore.username }}<el-icon class="el-icon--right"><arrow-down /></el-icon
-        ></el-button>
+        <div class="right-menu-avatar">
+          <el-icon :size="20">
+            <Setting />
+          </el-icon>
+        </div>
         <template #dropdown>
           <el-dropdown-menu>
-            <a target="_blank" href="https://github.com/pddzl/td27-admin">
-              <el-dropdown-item>GitHub</el-dropdown-item>
-            </a>
-            <el-dropdown-item @click="toPersonal">个人中心</el-dropdown-item>
+            <el-dropdown-item v-for="r in Routers" @click="toRoute(r.component)" style="width: 120px">
+              {{ r.label }}
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+      <!--     用户中心 -->
+      <el-dropdown class="right-menu-item">
+        <div class="right-menu-avatar">
+          <el-avatar :icon="UserFilled" :size="20" />
+          <span>{{ userStore.username }}</span>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item @click="toRoute('Profile')">个人中心</el-dropdown-item>
             <el-dropdown-item divided @click="logout">
               <span style="display: block">退出登录</span>
             </el-dropdown-item>
@@ -36,6 +50,7 @@ import ThemeSwitch from "@/components/ThemeSwitch/index.vue"
 import Screenfull from "@/components/Screenfull/index.vue"
 import { joinInBlacklistApi } from "@/api/system/jwt"
 import { useSettingsStore } from "@/store/modules/settings"
+import { UserFilled } from "@element-plus/icons-vue"
 
 const router = useRouter()
 const appStore = useAppStore()
@@ -44,6 +59,14 @@ const userStore = useUserStore()
 
 const { sidebar } = storeToRefs(appStore)
 const { showThemeSwitch, showScreenfull } = storeToRefs(settingsStore)
+
+/* 跳转路由 */
+const Routers = [
+  { label: "用户管理", component: "User" },
+  { label: "角色管理", component: "Role" },
+  { label: "菜单管理", component: "Menu" },
+  { label: "API 管理", component: "Api" }
+]
 
 /** 切换侧边栏 */
 const toggleSidebar = () => {
@@ -61,8 +84,8 @@ const logout = () => {
     .catch(() => {})
 }
 
-const toPersonal = () => {
-  router.push({ name: "Profile" })
+const toRoute = (componentName: string) => {
+  router.push({ name: componentName })
 }
 </script>
 
@@ -70,7 +93,8 @@ const toPersonal = () => {
 .navigation-bar {
   height: var(--base-navigationbar-height);
   overflow: hidden;
-  background: var(--base-header-bg-color);
+  background: #fff;
+
   .hamburger {
     display: flex;
     align-items: center;
@@ -79,6 +103,7 @@ const toPersonal = () => {
     padding: 0 15px;
     cursor: pointer;
   }
+
   .breadcrumb {
     float: left;
     // 参考 Bootstrap 的响应式设计将宽度设置为 576
@@ -86,6 +111,7 @@ const toPersonal = () => {
       display: none;
     }
   }
+
   .right-menu {
     float: right;
     margin-right: 10px;
@@ -93,12 +119,27 @@ const toPersonal = () => {
     display: flex;
     align-items: center;
     color: #606266;
+
     .right-menu-item {
       padding: 0 10px;
       cursor: pointer;
+
+      .right-menu-avatar {
+        display: flex;
+        align-items: center;
+
+        .el-avatar {
+          margin-right: 10px;
+        }
+
+        span {
+          font-size: 16px;
+        }
+      }
     }
   }
 }
+
 .el-button.is-plain {
   --el-button-hover-border-color: #ffffff;
 }
