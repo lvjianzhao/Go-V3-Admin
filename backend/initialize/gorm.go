dacab9a8d9757ef11ea069594a8e7c9a4e4ae6e2
lvjianzhao <lv916551516@163.com>
2023-07-24 19:19:55 +0800

    style(backend): 修改gorm的日志级别和zap的联动

--- backend/initialize/gorm.go
+++ backend/initialize/gorm.go
@@ -33,11 +33,16 @@ func (w *writer) Printf(message string, data ...interface{}) {
 }
 
 func gormConfig() *gorm.Config {
+	logLevel := logger.Warn
+	if global.CONFIG.Zap.Level == "debug" {
+		logLevel = logger.Info
+	}
+
 	newLogger := logger.New(
 		NewWriter(log.New(os.Stdout, "\r\n", log.LstdFlags)), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
 		logger.Config{
 			SlowThreshold:             200 * time.Millisecond, // 慢 SQL 阈值
-			LogLevel:                  logger.Warn,            // 日志级别
+			LogLevel:                  logLevel,               // 日志级别
 			IgnoreRecordNotFoundError: true,                   // 忽略ErrRecordNotFound（记录未找到）错误
 			Colorful:                  false,                  // 禁用彩色打印
 		},
